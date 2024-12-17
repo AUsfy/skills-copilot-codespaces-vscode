7051237ea67a8259d44fdbd64a3d32bf2116d93d
Abdellatif EL YOUSSEFY <abdellatif.elyoussefy@gmail.com>
2024-12-17 16:58:03 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,21 +1,6 @@
-// create a web server using express
-const express = require('express');
-const app = express();
-// create a port to listen to
-const port = 3000;
-
-// import the comments data
-const comments = require('./comments-data');
-
-// use the express.static built-in middleware function to serve static files
-app.use(express.static('public'));
-
-// create a route to get the comments data
-app.get('/comments', (req, res) => {
-  res.json(comments);
-});
-
-// start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// create web server and listen on port 3000
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var comments = require('./comments.json')
