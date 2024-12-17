23c597499de1b2c64a9598603da9442b5d6b20f4
Abdellatif EL YOUSSEFY <abdellatif.elyoussefy@gmail.com>
2024-12-17 16:54:33 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,42 +1,21 @@
-// create a web server that listens on port 3000
-// when it receives a request to /comments, it should return
-// an array of comments
-// when it receives a request to /comments/1, it should return
-// the comment with id 1
-// when it receives a request to /comments/2, it should return
-// the comment with id 2
-// when it receives a request to /comments/3, it should return
-// the comment with id 3
-// when it receives a request to /comments/4, it should return
-// the comment with id 4
+// create a web server using express
+const express = require('express');
+const app = express();
+// create a port to listen to
+const port = 3000;
 
-var http = require('http');
-var server = http.createServer(function(req, res) {
-  if (req.url === '/comments') {
-    res.write(JSON.stringify([
-      { id: 1, author: 'user1', body: 'comment 1' },
-      { id: 2, author: 'user2', body: 'comment 2' },
-      { id: 3, author: 'user3', body: 'comment 3' },
-      { id: 4, author: 'user4', body: 'comment 4' }
-    ]));
-    res.end();
-  } else if (req.url === '/comments/1') {
-    res.write(JSON.stringify({ id: 1, author: 'user1', body: 'comment 1' }));
-    res.end();
-  } else if (req.url === '/comments/2') {
-    res.write(JSON.stringify({ id: 2, author: 'user2', body: 'comment 2' }));
-    res.end();
-  } else if (req.url === '/comments/3') {
-    res.write(JSON.stringify({ id: 3, author: 'user3', body: 'comment 3' }));
-    res.end();
-  } else if (req.url === '/comments/4') {
-    res.write(JSON.stringify({ id: 4, author: 'user4', body: 'comment 4' }));
-    res.end();
-  } else {
-    res.statusCode = 404;
-    res.end();
-  }
+// import the comments data
+const comments = require('./comments-data');
+
+// use the express.static built-in middleware function to serve static files
+app.use(express.static('public'));
+
+// create a route to get the comments data
+app.get('/comments', (req, res) => {
+  res.json(comments);
 });
-server.listen(3000);
 
-// Exercise 2
+// start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
